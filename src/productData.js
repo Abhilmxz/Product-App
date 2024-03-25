6b4e4e430a49484fd0565a25b4e5d6c2beae3bea
Abhishek <abhishekonline45@gmail.com>
2024-03-25 12:54:12 +0530

    first commit

---
+++ src/productData.js
@@ -0,0 +1,119 @@
+import c1 from "./img/c1.jpg"
+import c2 from "./img/c2.jpg"
+import c3 from "./img/c3.jpg"
+import c4 from "./img/c4.jpg"
+
+// FOOT WEAR IMG
+import S1 from "./img/S1.jpg"
+import S2 from "./img/S2.jpg"
+import S3 from "./img/S3.jpg"
+import S4 from "./img/S4.jpg"
+
+// MENS WEAR IMG
+import T1 from "./img/T1.jpg"
+import T2 from "./img/T2.jpg"
+import T3 from "./img/T3.jpg"
+import T4 from "./img/T4.jpg"
+
+
+const ProductData=[
+
+    {
+      id:1,
+      Title:'Coffee Cup',
+      img: c1,
+      Price: 200,
+      Des:"There are various materials used in coffee mugs, including ceramic, glass, stoneware, stainless steel, China, and melamine.",
+      cat:'mug',
+    },
+    {
+      id:2,
+      Title:'Printed Cup',
+      img: c2,
+      Price: 250,
+      Des:"There are various materials used in coffee mugs, including ceramic, glass, stoneware, stainless steel, China, and melamine.",
+      cat:'mug',
+    },
+    {
+      id:3,
+      Title:'Coffee Cup',
+      img:c3,
+      Price: 150,
+      Des:"There are various materials used in coffee mugs, including ceramic, glass, stoneware, stainless steel, China, and melamine.",
+      cat:'mug',
+    },
+    {
+      id:4,
+      Title:'Black Cup',
+      img: c4,
+      Price: 210,
+      Des:"There are various materials used in coffee mugs, including ceramic, glass, stoneware, stainless steel, China, and melamine.",
+      cat:'mug',
+    },
+    {
+      id:5,
+      Title:'Printed Shoes',
+      img: S1,
+      Price: 999,
+      Des:"There are various materials used in coffee mugs, including ceramic, glass, stoneware, stainless steel, China, and melamine.",
+      cat:'shoe',
+    },
+    {
+      id:6,
+      Title:'Air Yellow shoes',
+      img: S2,
+      Price: 999,
+      Des:"There are various materials used in coffee mugs, including ceramic, glass, stoneware, stainless steel, China, and melamine.",
+      cat:'shoe',
+    },
+    {
+      id:7,
+      Title:'mens shoes',
+      img: S3,
+      Price: 999,
+      Des:"There are various materials used in coffee mugs, including ceramic, glass, stoneware, stainless steel, China, and melamine.",
+      cat:'shoe',
+    },
+    {
+      id:8,
+      Title:'mens shoes',
+      img:S4,
+      Price: 999,
+      Des:"There are various materials used in coffee mugs, including ceramic, glass, stoneware, stainless steel, China, and melamine.",
+      cat:'shoe',
+    },
+    {
+      id:9,
+      Title:'black t-shirt',
+      img: T1,
+      Price: 499,
+      Des:"There are various materials used in coffee mugs, including ceramic, glass, stoneware, stainless steel, China, and melamine.",
+      cat:'tshirt',
+    },
+    {
+      id:10,
+      Title:'darkblue t-shirt',
+      img: T2,
+      Price: 499,
+      Des:"There are various materials used in coffee mugs, including ceramic, glass, stoneware, stainless steel, China, and melamine.",
+      cat:'tshirt',
+    },
+    {
+      id:11,
+      Title:'lgreen t-shirt',
+      img: T3,
+      Price: 499,
+      Des:"There are various materials used in coffee mugs, including ceramic, glass, stoneware, stainless steel, China, and melamine.",
+      cat:'tshirt',
+    },
+    {
+      id:12,
+      Title:'lpink t-shirt',
+      img: T4,
+      Price: 499,
+      Des:"There are various materials used in coffee mugs, including ceramic, glass, stoneware, stainless steel, China, and melamine.",
+      cat:'tshirt',
+    },
+];
+
+export default ProductData;
